19b637d7dab91559cfffbcedb056c0eb1df33637
Stanislav Muhametsin <stanislav.muhametsin@cometasolutions.fi>
2015-08-14 01:24:15 +0300

    CallNuGetExecutableTask now works in inline mode as well.

--- CLR/Source/CommonBuildTools/CallNuGetExecutableTask.cs
+++ CLR/Source/CommonBuildTools/CallNuGetExecutableTask.cs
@@ -25,6 +25,43 @@ using System.Text;
 
 namespace CommonBuildTools
 {
+   // This class is here because CallNuGetExecutableTask is here as well (and it uses DownloadFileTask class).
+   public class DownloadFileTask : Task
+   {
+      [Required]
+      public String URI { get; set; }
+
+      [Required]
+      public String FilePath { get; set; }
+
+      public override Boolean Execute()
+      {
+         return DoDownload( this.URI, this.FilePath, this.Log );
+      }
+
+      internal static Boolean DoDownload( String uri, String filePath, TaskLoggingHelper log )
+      {
+         var retVal = false;
+         try
+         {
+            var fn = System.IO.Path.GetFullPath( filePath );
+
+            log.LogMessage( MessageImportance.High, "Downloading from {0} to {1}.", uri, fn );
+            using ( var webClient = new System.Net.WebClient() )
+            {
+               webClient.DownloadFile( uri, fn );
+            }
+            retVal = true;
+         }
+         catch ( Exception exc )
+         {
+            log.LogErrorFromException( exc );
+         }
+
+         return retVal;
+      }
+   }
+
    public class CallNuGetExecutableTask : ToolTask
    {
       public String NuGetExecutable { get; set; }

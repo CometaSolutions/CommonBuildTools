96b798c70b795c49b956184a36a279a5449dc2f9
Stanislav Muhametsin <stanislav.muhametsin@cometasolutions.fi>
2015-08-15 17:43:35 +0300

    Some work on NuGet tasks.

---
+++ CLR/Source/CommonBuildTools/NUnitTask.cs
@@ -0,0 +1,160 @@
+﻿/*
+ * Copyright 2015 Stanislav Muhametsin. All rights Reserved.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+using System.IO;
+
+namespace CommonBuildTools
+{
+   // For some reason, MSBuild.Community.Tasks.NUnit task does not have customizable version (only way to customize that is through ToolPath, which doesn't work well in version controlled environment...)
+   public class NUnitTask : ToolTask
+   {
+      private const String REG_KEY = @"HKEY_CURRENT_USER\Software\nunit.org\Nunit\";
+      private const String DEFAULT_NUNIT_VERSION = "2.6.3";
+
+      public ITaskItem[] Assemblies { get; set; }
+
+      public String ConfigurationFile { get; set; }
+
+      public String IncludeCategories { get; set; }
+
+      public String ExcludeCategories { get; set; }
+
+      public String StandardOutputFile { get; set; }
+
+      public String ErrorOutputFile { get; set; }
+
+      public Boolean Labels { get; set; }
+
+      public String TestOutputFile { get; set; }
+
+      public String TestOutputXSLTFile { get; set; }
+
+      public String NUnitVersion { get; set; }
+
+      public Boolean NoShadowAssemblies { get; set; }
+
+      public Boolean RawXMLOutput { get; set; }
+
+      public Boolean Use32BitProcess { get; set; }
+
+      protected override String GenerateFullPathToTool()
+      {
+         var filePath = this.ToolPath;
+
+         if ( String.IsNullOrEmpty( filePath ) )
+         {
+            // Try to deduce
+            var version = this.NUnitVersion;
+            if ( String.IsNullOrEmpty( version ) )
+            {
+               // Most common at the moment most likely 2.6.3
+               version = DEFAULT_NUNIT_VERSION;
+            }
+
+            var tmp = Path.Combine(
+               Environment.GetFolderPath( Environment.SpecialFolder.ProgramFilesX86 ),
+               "NUnit " + version
+               );
+            if ( !Directory.Exists( tmp ) )
+            {
+               // Try registry
+               try
+               {
+                  tmp = Convert.ToString( Microsoft.Win32.Registry.GetValue( REG_KEY + version, "InstallDir", tmp ) );
+               }
+               catch ( Exception exc )
+               {
+                  this.Log.LogError( "Error when getting NUnit {0} install directory from registry: {1}.", version, exc.Message );
+               }
+            }
+
+            if ( !String.IsNullOrEmpty( tmp ) && Directory.Exists( tmp ) )
+            {
+               filePath = Path.Combine( filePath, "bin" );
+            }
+         }
+
+         return filePath;
+      }
+
+      protected override String ToolName
+      {
+         get
+         {
+            var exe = "nunit-console";
+            if ( this.Use32BitProcess )
+            {
+               exe += "-x86";
+            }
+
+            if ( Environment.OSVersion.Platform != PlatformID.Unix )
+            {
+               exe += ".exe";
+            }
+
+            return exe;
+         }
+      }
+
+      protected override String GenerateCommandLineCommands()
+      {
+         var cmd = new CommandLineBuilder();
+
+         // First, assemblies
+         cmd.AppendFileNamesIfNotNull( this.Assemblies, " " );
+
+         // Then, options
+         if ( this.NoShadowAssemblies )
+         {
+            cmd.AppendSwitch( "/noshadow" );
+         }
+
+         if ( this.Labels )
+         {
+            cmd.AppendSwitch( "/labels" );
+         }
+
+         if ( this.RawXMLOutput )
+         {
+            cmd.AppendSwitch( "/xmlconsole" );
+         }
+
+         cmd.AppendSwitchIfNotNull( "/config:", this.ConfigurationFile );
+
+         cmd.AppendSwitchIfNotNull( "/include:", this.IncludeCategories );
+
+         cmd.AppendSwitchIfNotNull( "/exclude:", this.ExcludeCategories );
+
+         cmd.AppendSwitchIfNotNull( "/out:", this.StandardOutputFile );
+
+         cmd.AppendSwitchIfNotNull( "/err:", this.ErrorOutputFile );
+
+         cmd.AppendSwitchIfNotNull( "/xml:", this.TestOutputFile );
+
+         cmd.AppendSwitchIfNotNull( "/transform:", this.TestOutputXSLTFile );
+
+         return cmd.ToString();
+
+      }
+   }
+}
--- CLR/Source/CommonBuildTools/NuGetRelated.cs
+++ CLR/Source/CommonBuildTools/NuGetRelated.cs
@@ -15,39 +15,177 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. 
  */
-using CommonBuildTools;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.IO;
+using CommonBuildTools;
 using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace CommonBuildTools
 {
-   public class CreateNuGetPackagesFilesTask : Task
+   // This class is here because CallNuGetExecutableTask is here as well (and it uses DownloadFileTask class).
+   public class DownloadFileTask : Task
    {
+      [Required]
+      public String URI { get; set; }
 
+      [Required]
+      public String FilePath { get; set; }
 
+      public override Boolean Execute()
+      {
+         return DoDownload( this.URI, this.FilePath, this.Log );
+      }
+
+      internal static Boolean DoDownload( String uri, String filePath, TaskLoggingHelper log )
+      {
+         var retVal = false;
+         try
+         {
+            var fn = System.IO.Path.GetFullPath( filePath );
+
+            log.LogMessage( MessageImportance.High, "Downloading from {0} to {1}.", uri, fn );
+            using ( var webClient = new System.Net.WebClient() )
+            {
+               webClient.DownloadFile( uri, fn );
+            }
+            retVal = true;
+         }
+         catch ( Exception exc )
+         {
+            log.LogErrorFromException( exc );
+         }
+
+         return retVal;
+      }
+   }
+
+   public abstract class AbstractNuGetTask : ToolTask
+   {
+      public String NuGetExecutable { get; set; }
+
+      protected override String GenerateFullPathToTool()
+      {
+         var nugetExe = this.NuGetExecutable;
+         if ( String.IsNullOrEmpty( nugetExe ) )
+         {
+            nugetExe = Path.Combine( Environment.CurrentDirectory, "NuGet.exe" );
+            this.Log.LogMessage( MessageImportance.High, "A path to NuGet executable was not specified, using {0}.", nugetExe );
+         }
+         else
+         {
+            nugetExe = Path.GetFullPath( nugetExe );
+         }
+
+         if ( !File.Exists( nugetExe ) )
+         {
+            var uri = "https://www.nuget.org/nuget.exe";
+            this.Log.LogMessage( MessageImportance.High, "NuGet executable {0} does not exist, downloading from {1}.", nugetExe, uri );
+            if ( !DownloadFileTask.DoDownload( uri, nugetExe, this.Log ) )
+            {
+               this.Log.LogWarning( "There was an error while downloading file, subsequent NuGet calls will most likely fail." );
+            }
+         }
+
+         return nugetExe;
+      }
+
+      protected override String ToolName
+      {
+         get
+         {
+            return "NuGet.exe";
+         }
+      }
+
+      protected override abstract String GenerateCommandLineCommands();
+   }
+
+   public class CallNuGetExecutableTask : AbstractNuGetTask
+   {
+      public String NuGetArguments { get; set; }
+
+      protected override String GenerateCommandLineCommands()
+      {
+         return this.NuGetArguments;
+      }
+   }
+
+   public class NuGetRestoreTask : AbstractNuGetTask
+   {
       public String NuGetManagementFile { get; set; }
       public String NuGetManagementContents { get; set; }
 
-      [Output]
-      public ITaskItem[] NuGetPackagesConfigFiles { get; set; }
+      private NuGetManagement _nugetManagement;
+      private PackagesFileInfo? _currentPackage;
+
+      protected override String GenerateCommandLineCommands()
+      {
+         var ngm = this._nugetManagement;
+         var pkgNullable = this._currentPackage;
+         if ( ngm != null && pkgNullable.HasValue )
+         {
+            var pkg = pkgNullable.Value;
+            return ngm.CreateNuGetParameters( pkg );
+         }
+         else
+         {
+            this.Log.LogError( "Internal error, no info to generate NuGet command line arguments from." );
+            throw new Exception();
+         }
+      }
 
       public override Boolean Execute()
       {
          var retVal = false;
-         var file = this.NuGetManagementFile;
+         // First, deduce how many packages.config files we need to create
+         var infos = this.CreatePackagesFileInfos( out this._nugetManagement );
+         if ( infos != null
+            && infos.Count > 0
+            && this.CreatePackageConfigFiles( infos )
+            )
+         {
+            // Then execute NuGet x times
+            var seenError = false;
+            foreach ( var info in infos )
+            {
+               this._currentPackage = info;
+               try
+               {
+                  if ( !base.Execute() )
+                  {
+                     seenError = true;
+                  }
+               }
+               catch
+               {
+                  seenError = true;
+               }
+            }
 
+            retVal = !seenError;
+         }
+
+         return retVal;
+
+      }
+
+      private List<PackagesFileInfo> CreatePackagesFileInfos( out NuGetManagement ngm )
+      {
+         var nugetManagementFile = this.NuGetManagementFile;
          XDocument nuGetManagementDoc = null;
 
-         if ( String.IsNullOrEmpty( file ) )
+         String ngmLocation;
+         if ( String.IsNullOrEmpty( nugetManagementFile ) )
          {
-            var contens = this.NuGetManagementContents;
-            if ( String.IsNullOrEmpty( contens ) )
+            ngmLocation = Environment.CurrentDirectory;
+            var nugetManagementContents = this.NuGetManagementContents;
+            if ( String.IsNullOrEmpty( nugetManagementContents ) )
             {
                this.Log.LogError( "Either management file path or file contents must be specified." );
             }
@@ -55,7 +193,7 @@ namespace CommonBuildTools
             {
                try
                {
-                  nuGetManagementDoc = XDocument.Parse( contens );
+                  nuGetManagementDoc = XDocument.Parse( nugetManagementContents );
                }
                catch ( Exception e )
                {
@@ -65,57 +203,38 @@ namespace CommonBuildTools
          }
          else
          {
+            ngmLocation = Path.GetDirectoryName( nugetManagementFile );
             try
             {
-               nuGetManagementDoc = XDocument.Load( file );
+               nuGetManagementDoc = XDocument.Load( nugetManagementFile );
             }
             catch ( Exception e )
             {
-               this.Log.LogError( "Failed to load NuGet management configuration from {0}: {1}.", file, e.Message );
+               this.Log.LogError( "Failed to load NuGet management configuration from {0}: {1}.", nugetManagementFile, e.Message );
             }
          }
 
+         List<PackagesFileInfo> packageInfos = null;
+         ngm = null;
          if ( nuGetManagementDoc != null )
          {
-            List<PackagesFileInfo> packageInfos = null;
             try
             {
-               var ngm = nuGetManagementDoc.Root.CreateNuGetManagement();
-               packageInfos = this.ExtractPackagesFileInfos( ngm );
-
-               this.NuGetPackagesConfigFiles = packageInfos
-                  .Select( pkg => ngm.CreateTaskItem( pkg ) )
-                  .ToArray();
-               this.CreatePackageConfigFiles( packageInfos );
-
-               retVal = true;
+               ngm = nuGetManagementDoc
+                  .XPathSelectElement( "//NuGetManagement" )
+                  .CreateNuGetManagement( ngmLocation );
+               if ( ngm != null )
+               {
+                  packageInfos = this.ExtractPackagesFileInfos( ngm );
+               }
             }
             catch ( Exception e )
             {
                this.Log.LogError( "Failed to process NuGet management: {0}.", e.Message );
             }
-
-            if ( !retVal && packageInfos != null )
-            {
-               // Clean-up generated files
-               foreach ( var fn in packageInfos.Select( pkg => pkg.PackagesConfigPath ) )
-               {
-                  if ( File.Exists( fn ) )
-                  {
-                     try
-                     {
-                        File.Delete( fn );
-                     }
-                     catch
-                     {
-                        // Ignore
-                     }
-                  }
-               }
-            }
          }
 
-         return retVal;
+         return packageInfos;
       }
 
       private List<PackagesFileInfo> ExtractPackagesFileInfos( NuGetManagement ngm )
@@ -157,19 +276,35 @@ namespace CommonBuildTools
             .ToList();
       }
 
-      private void CreatePackageConfigFiles( IEnumerable<PackagesFileInfo> packageConfigs )
+      private Boolean CreatePackageConfigFiles( IEnumerable<PackagesFileInfo> packageConfigs )
       {
+         var retVal = true;
          foreach ( var cfg in packageConfigs )
          {
-            new XDocument(
-               new XDeclaration( "1.0", "utf-8", "yes" ),
-               new XElement( "packages", cfg.Packages.Select( pkg =>
-                  new XElement( "package", new XAttribute( "id", pkg.PackageID ), new XAttribute( "version", pkg.PackageVersion ) )
-                  ) )
-               ).Save( cfg.PackagesConfigPath );
+            var curFile = cfg.PackagesConfigPath;
+            this.Log.LogMessage( MessageImportance.High, "Storing packages {0} to {1}.", String.Join( ", ", cfg.Packages.Select( pkg => pkg.PackageID + ":" + pkg.PackageVersion ) ), curFile );
+
+            try
+            {
+               new XDocument(
+                  new XDeclaration( "1.0", "utf-8", "yes" ),
+                  new XElement( "packages", cfg.Packages.Select( pkg =>
+                     new XElement( "package", new XAttribute( "id", pkg.PackageID ), new XAttribute( "version", pkg.PackageVersion ) )
+                     ) )
+                  ).Save( curFile );
+            }
+            catch ( Exception exc )
+            {
+               this.Log.LogError( "Error while saving packages.config file to {0}: {1}.", curFile, exc.Message );
+               retVal = false;
+               break;
+            }
          }
+
+         return retVal;
       }
    }
+
 
    internal class NuGetManagement
    {
@@ -355,27 +490,27 @@ namespace CommonBuildTools
 
 internal static partial class E_CBT
 {
-   public static NuGetManagement CreateNuGetManagement( this XElement element )
+   public static NuGetManagement CreateNuGetManagement( this XElement element, String ngmLocation )
    {
       return element == null ?
          null :
-         new NuGetManagement( element.ListOrEmpty( "Pacakges", "Package" ).Select( el => el.CreateNuGetPackage() ) )
+         new NuGetManagement( element.ListOrEmpty( "Pacakges", "Package" ).Select( el => el.CreateNuGetPackage( ngmLocation ) ) )
          {
-            GlobalConfiguration = (GlobalNuGetConfiguration) element.Element( "GlobalConfiguration" ).CreateNuGetConfiguration( true )
+            GlobalConfiguration = (GlobalNuGetConfiguration) element.Element( "GlobalConfiguration" ).CreateNuGetConfiguration( true, ngmLocation )
          };
    }
-   public static NuGetPackage CreateNuGetPackage( this XElement element )
+   public static NuGetPackage CreateNuGetPackage( this XElement element, String ngmLocation )
    {
       return element == null ?
          null :
          new NuGetPackage( element.ListOrEmpty( "Versions", "Version" ).Select( el => el.Value ) )
          {
             PackageID = element.ValueOrNull( "ID" ),
-            PackageSpecificConfiguration = (PackageSpecificConfiguration) element.Element( "PackageSpecificConfiguration" ).CreateNuGetConfiguration( false )
+            PackageSpecificConfiguration = (PackageSpecificConfiguration) element.Element( "PackageSpecificConfiguration" ).CreateNuGetConfiguration( false, ngmLocation )
          };
    }
 
-   public static AbstractNuGetConfiguration CreateNuGetConfiguration( this XElement element, Boolean isGlobal )
+   public static AbstractNuGetConfiguration CreateNuGetConfiguration( this XElement element, Boolean isGlobal, String ngmLocation )
    {
       AbstractNuGetConfiguration retVal;
       if ( element == null )
@@ -392,18 +527,18 @@ internal static partial class E_CBT
             } :
             new PackageSpecificConfiguration( sources );
 
-         element.InitializeAbstractNuGetConfiguration( retVal );
+         element.InitializeAbstractNuGetConfiguration( retVal, ngmLocation );
       }
 
       return retVal;
    }
 
-   private static void InitializeAbstractNuGetConfiguration( this XElement element, AbstractNuGetConfiguration config )
+   private static void InitializeAbstractNuGetConfiguration( this XElement element, AbstractNuGetConfiguration config, String ngmLocation )
    {
       if ( element != null && config != null )
       {
-         config.PackagesDirectory = element.ValueOrNull( "PackagesDirectory" );
-         config.ConfigurationFile = element.ValueOrNull( "ConfigurationFile" );
+         config.PackagesDirectory = element.ValueOrNull( "PackagesDirectory" ).ConvertRelativeToAbsolute( ngmLocation );
+         config.ConfigurationFile = element.ValueOrNull( "ConfigurationFile" ).ConvertRelativeToAbsolute( ngmLocation );
          var noCacheElement = element.ValueOrNull( "NoCache" );
          Boolean? noCache;
          if ( noCacheElement == null )
@@ -509,49 +644,57 @@ internal static partial class E_CBT
       return retVal.HasValue && retVal.Value;
    }
 
-   internal static ITaskItem CreateTaskItem( this NuGetManagement ngm, PackagesFileInfo info )
+   internal static String CreateNuGetParameters( this NuGetManagement ngm, PackagesFileInfo info )
    {
-      var sb = new StringBuilder();
-      sb.Append( "\"" )
-         .Append( info.PackagesConfigPath )
-         .Append( "\" -PackagesDirectory \"" )
-         .Append( info.OutputDirectory )
-         .Append( "\" " );
+      var cmd = new CommandLineBuilder();
+      cmd.AppendTextUnquoted( "restore " );
 
-      if ( info.Sources.Count > 0 )
-      {
-         sb.Append( "-Source \"" )
-            .Append( String.Join( ";", info.Sources ) )
-            .Append( "\" " );
-      }
+      cmd.AppendFileNameIfNotNull( info.PackagesConfigPath );
+
+      cmd.AppendSwitchIfNotNull( "-PackagesDirectory", info.OutputDirectory );
+
+      cmd.AppendSwitchIfNotNull( "-Source", info.Sources.ToArray().NullIfEmpty(), ";" );
 
       if ( info.NoCache )
       {
-         sb.Append( "-NoCache " );
+         cmd.AppendSwitch( "-NoCache" );
       }
 
-      if ( !String.IsNullOrEmpty( info.NuGetConfigPath ) )
-      {
-         sb.Append( "-ConfigFile \"" )
-            .Append( info.NuGetConfigPath )
-            .Append( "\" " );
-      }
+      cmd.AppendSwitchIfNotNull( "-ConfigFile", info.NuGetConfigPath.NullIfEmpty() );
 
       if ( ngm.GlobalConfiguration != null && ngm.GlobalConfiguration.DisableParallelProcessing )
       {
-         sb.Append( "-DisableParallelProcsessing " );
+         cmd.AppendSwitch( "-DisableParallelProcsessing" );
       }
 
-      sb.Append( "-Verbosity detailed -NonInteractive" );
+      cmd.AppendTextUnquoted( "-Verbosity detailed -NonInteractive" );
 
-      var retVal = new TaskItem( info.PackagesConfigPath );
-      retVal.SetMetadata( "NuGetParameters", sb.ToString() );
-      return retVal;
+      return cmd.ToString();
    }
 
    private static Boolean ParseAsBooleanSafe( this String str )
    {
       Boolean parsedBoolean;
       return Boolean.TryParse( str, out parsedBoolean ) && parsedBoolean;
+   }
+
+   private static String NullIfEmpty( this String str )
+   {
+      return str != null && str.Length == 0 ? null : str;
+   }
+
+   private static T[] NullIfEmpty<T>( this T[] array )
+   {
+      return array != null && array.Length == 0 ? null : array;
+   }
+
+   private static String ConvertRelativeToAbsolute( this String str, String ngmLocation )
+   {
+      if ( !String.IsNullOrEmpty( str ) && !Path.IsPathRooted( str ) )
+      {
+         str = Path.Combine( ngmLocation, str );
+      }
+
+      return str;
    }
 }

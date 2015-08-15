962a2363200e87a957bec1f4a20226a8feb7fbfe
Stanislav Muhametsin <stanislav.muhametsin@cometasolutions.fi>
2015-08-16 01:49:29 +0300

    Correct namespaces to NuSpec file, also fixed boolean case.

--- CLR/Source/CommonBuildTools/NuGetRelated.cs
+++ CLR/Source/CommonBuildTools/NuGetRelated.cs
@@ -666,11 +666,22 @@ namespace CommonBuildTools
             this.AddTargetFWAssemblies( md );
 
             // Top-level element
-            XNamespace ns = "http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd";
-            var nuspec = new XElement( ns + "package", md );
+            var nuspec = new XElement( "package", md );
 
             // Files
             this.AddFiles( nuspec );
+
+            // Change namespace of all elements and attributes
+            XNamespace ns = "http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd";
+            foreach ( var el in nuspec.XPathSelectElements( "//*" ).ToArray() )
+            {
+               el.Name = ns + el.Name.LocalName;
+               foreach ( var attr in el.Attributes().ToArray() )
+               {
+                  el.SetAttributeValue( attr.Name, null );
+                  el.SetAttributeValue( ns + attr.Name.LocalName, attr.Value );
+               }
+            }
 
             // Save
             var outDir = Path.GetDirectoryName( outputPath );
@@ -736,7 +747,8 @@ namespace CommonBuildTools
       {
          if ( content.HasValue )
          {
-            AddElement( parent, elementName, content.Value.ToString() );
+            // .ToString will return 'True' or 'False', which will violate case-sensitive NuGet XSD schema rules
+            AddElement( parent, elementName, content.Value ? "true" : "false" );
          }
       }
 
